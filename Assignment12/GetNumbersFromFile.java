7439fc9a69670602411ac5cfc10f10f0c4da766d
Eversco <36009753+Eversco@users.noreply.github.com>
2018-04-20 11:46:02 -0700

    Assignement 12

--- Assignment12/GetNumbersFromFile.java
+++ Assignment12/GetNumbersFromFile.java
@@ -7,12 +7,53 @@ class GetNumbersFromFile {
 
         File file = new File("data.txt");
         Scanner scan = new Scanner(file);
-        int data = 0;
+        int age, iq, gender, height;
+        int totalPeople = 0;
+        int totalFemale = 0;
+        int totalMale = 0;
+        int tallSmartDudes = 0; // Tall smart dudes who get all the chicks
+        int totalIQ = 0;
+        int totalHeight = 0;
+        int youngestAge = Integer.MAX_VALUE;
+        int oldestAge = 0;
 
-        while (data != -1) {
-            data = scan.nextInt();
-            System.out.println(data);
+         while (scan.hasNextInt()) {
+         age = scan.nextInt();
+         if (age == -1) { // if enter -1 quit program
+             break;
+            }
+            iq = scan.nextInt();
+            gender = scan.nextInt();
+            height = scan.nextInt();
+            if (age > oldestAge){ // if any age is older than the current oldest, the oldest equals that age
+                oldestAge = age;
+            }
+            if (age < youngestAge){// if any age is younger than the current youngest, the youngest equals that age
+                youngestAge = age;
+            }
+            if (gender == 1) { //more males as much the user enters
+                totalMale++;
+                if (height > 72 && iq > 140){ // if any people taller than 6ft and higher than IQ 140, tall smart dudes confirmed
+                    tallSmartDudes++;
+                }
+            } else if (gender == 0) {
+                totalFemale++;
+            }
+            totalIQ += iq; // adding all IQs
+            totalHeight += height;// adding all height
+            totalPeople++;
+         System.out.println("");
         }
+        int averageIQ = totalIQ / totalPeople; // making out average of IQ
+        int averageHeight = totalHeight / totalPeople;// making out the average of height
+        System.out.println("Total People: " + totalPeople);
+        System.out.println("Males: " + totalMale);
+        System.out.println("Females: " + totalFemale);
+        System.out.println("Males over 6ft and greater than IQ 140: " + tallSmartDudes);
+        System.out.println("Average IQ: " + averageIQ);
+        System.out.println("Average Height: " + averageHeight);
+        System.out.println("Oldest: " + oldestAge);
+        System.out.println("Youngest: " + youngestAge);
         scan.close();
     }
 }
--- Assignment12/PrintNumbersToFile.java
+++ Assignment12/PrintNumbersToFile.java
@@ -5,14 +5,32 @@ class PrintNumbersToFile {
 
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
-        int data = 0;
-        File file = new File("data.txt");
-        PrintStream print = new PrintStream(file);
-
-        while (data != -1) {
-            System.out.print("DATA(-1 to exit): ");
-            data = scan.nextInt();
-            print.println(data);
+        int age, iq, gender, height;
+        File file = new File("data.txt"); //making new file
+        PrintStream print = new PrintStream(file);// adding dqta into file
+        age = 0;
+        while (age != -1) {
+            System.out.print("(Enter -1 into age to exit): ");
+            System.out.println("Enter the age: ");
+            age = scan.nextInt(); // enter age
+            if (age == -1){ // if age = -1 then program ends
+                break;
+            }
+            print.println(age); // print out age
+            System.out.println("Enter the IQ: ");
+            iq = scan.nextInt();
+            print.println(iq);
+            System.out.println("Enter Gender(1 for male, 0 for female):");
+            gender = scan.nextInt();
+            if (gender == 1){ // enter 1 print out male
+            print.println(1);
+        }
+        else if (gender == 0) { // if enter 0 but actually doesn't matter, prints out female
+            print.println(0);
+        }
+        System.out.println("Enter height:");
+        height = scan.nextInt();
+        print.println(height);
         }
 
         print.close();
